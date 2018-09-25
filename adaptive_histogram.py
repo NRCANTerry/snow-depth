8541c174e62ebe2bb2f24aebc28662258c73cf19
NRCANTerry <43354925+NRCANTerry@users.noreply.github.com>
2018-09-25 15:37:13 -0400

    Changes to adaptive histogram algorithm

--- adaptive_histogram.py
+++ adaptive_histogram.py
@@ -7,12 +7,13 @@ channels = cv2.split(img2)
 print len(channels)
 
 # create a CLAHE object
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl1 = clahe.apply(channels[0])
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5,5))
+channels[0] = clahe.apply(channels[0])
 
 cv2.merge(channels, img2)
 img2 = cv2.cvtColor(img2, cv2.COLOR_YCR_CB2BGR)
+vis = np.concatenate((img, img2), axis = 1)
+vis = cv2.resize(vis, None, fx = 0.5, fy = 0.5)
 
-cv2.imshow("1", img)
-cv2.imshow("2", img2)
+cv2.imshow("Comp", vis)
 cv2.waitKey()
