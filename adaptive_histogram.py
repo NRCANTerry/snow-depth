c4e8ae67481d8e3ef115f635b6479375c3f9351e
NRCANTerry <43354925+NRCANTerry@users.noreply.github.com>
2018-09-26 14:47:28 -0400

    Adaptive histogram equalization functions

--- adaptive_histogram.py
+++ adaptive_histogram.py
@@ -1,19 +1,51 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('IMG_0001.jpg')
-img2 = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
-channels = cv2.split(img2)
-print len(channels)
+def increase_brigthness(img, value):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	h, s, v = cv2.split(hsv)
 
-# create a CLAHE object
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(5,5))
-channels[0] = clahe.apply(channels[0])
+	lim = 255 - value
+	v[v > lim] = 255
+	v[v <= lim] += value
 
-cv2.merge(channels, img2)
-img2 = cv2.cvtColor(img2, cv2.COLOR_YCR_CB2BGR)
-vis = np.concatenate((img, img2), axis = 1)
-vis = cv2.resize(vis, None, fx = 0.5, fy = 0.5)
+	final_hsv = cv2.merge((h, s, v))
+	img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+	return img
 
-cv2.imshow("Comp", vis)
+def brighten_shadows(img, value):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	h, s, v = cv2.split(hsv)
+	print(np.amin(v))
+
+	v[v < 125] += value
+
+	final_hsv = cv2.merge((h, s, v))
+	img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+	return img
+
+# read in image
+img = cv2.imread('images/IMG_0003.jpg')
+
+# convert to LAB
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+# split image
+lab_planes = cv2.split(lab)
+
+# apply adaptive histogram equalization
+clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(8,8))
+lab_planes[0] = clahe.apply(lab_planes[0])
+
+# merge channels
+lab = cv2.merge(lab_planes)
+
+# convert to RGB
+bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+bgr2 = increase_brigthness(bgr, 30)
+
+vis = np.concatenate((img, bgr2), axis = 1)
+vis = cv2.resize(vis, None, fx = 0.25, fy = 0.25)
+
+cv2.imshow("Image", vis)
 cv2.waitKey()
---
+++ equalize.py
@@ -0,0 +1,63 @@
+# import necessary modules
+import numpy as np
+import cv2
+import os
+import sys
+
+# function to increase the brightness of an image
+def increase_brightness(img, val):
+	# convert image to HSV
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	h, s, v = cv2.split(hsv)
+
+	# increase value of pixels
+	lim = 255 - val
+	v[v > lim] = 255
+	v[v <= lim] += val
+
+	# merge channels
+	hsv_merge = cv2.merge((h,s,v))
+
+	# convert image to BGR and return
+	img = cv2.cvtColor(hsv_merge, cv2.COLOR_HSV2BGR)
+	return img
+
+# function to apply adaptive histogram equalization
+def equalize_hist(img_name, clip_limit, tile_size):	
+	# read in image
+	img = cv2.imread(img_name)
+
+	# convert to LAB
+	lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+	# split image
+	l, a, b = cv2.split(lab)
+
+	# apply adaptive histogram equalization
+	clahe = cv2.createCLAHE(clipLimit = clip_limit, tileGridSize = tile_size)
+	l = clahe.apply(l)
+
+	# merge channels
+	lab = cv2.merge((l,a,b))
+
+	# convert to BGR
+	bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+	# increase brightness
+	bgr = increase_brightness(bgr, 30)
+
+	# return image
+	return bgr
+
+# get images
+images = [file_name for file_name in os.listdir('./images')]
+
+for img_name in images:
+	img = cv2.imread('images/' + img_name)
+	img_mod = equalize_hist('images/' + img_name, 5.0, (8,8))
+
+	vis = np.concatenate((img, img_mod), axis = 1)
+	vis = cv2.resize(vis, None, fx = 0.25, fy = 0.25)
+
+	cv2.imshow("Image", vis)
+	cv2.waitKey()
