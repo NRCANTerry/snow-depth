c0dd0354fe95b4fef2f8bc271b93626af2a7681a
NRCANTerry <43354925+NRCANTerry@users.noreply.github.com>
2018-10-11 10:36:14 -0400

    Improve quality of registration (ECC)

--- includes/register.py
+++ includes/register.py
@@ -3,12 +3,11 @@ import cv2
 import numpy as np
 
 # global variables
-MAX_FEATURES = 5000
-GOOD_MATCH_PERCENT = 0.25
+MAX_FEATURES = 10000
+GOOD_MATCH_PERCENT = 0.05
 
 # function to align image to template
 def alignImages(img, template):
-
 	# apply median blur to highlight foreground featurse
 	img_blur = cv2.medianBlur(img, 5)
 	template_blur = cv2.medianBlur(template, 5)
@@ -32,7 +31,10 @@ def alignImages(img, template):
 	matches = sorted(matches, key = lambda x: x.distance)
 
 	# remove poor matches
-	matches = matches[:int(len(matches)*GOOD_MATCH_PERCENT)]
+	# keep matches with score less than 30 with a maximum of 100 matches
+	matches = [x for x in matches if x.distance <= 29]
+	if(len(matches) > 100):
+		matches = matches[:100]
 
 	# draw top matches
 	imgMatches = cv2.drawMatches(img, kp1, template, kp2, matches, None)
@@ -46,27 +48,107 @@ def alignImages(img, template):
 		points2[i, :] = kp2[match.trainIdx].pt
 
 	# convert numpy arrays to list
+	'''
 	points1 = points1.tolist()
 	points2 = points2.tolist()
 
-	# filter points to exclude poor point matching
-	# points which differ by more than 150 pixels in x or y domains
-	# are removed from the point lists
+	# determine ratio of y change to x change for each set of points
+	ratios = list()
+
+	# iterate through list of points
 	for i, point in enumerate(points1):
-		if(abs(point[1] - points2[i][1]) != 0 and abs(point[0] - points2[i][0]) > 150 and \
-		(abs(point[0] - points2[i][0]) / abs(point[1] - points2[i][1])) < 25):
-			points1.pop(i)
-			points2.pop(i)
+		# calculate ratio
+		ratios.append((point[1] - points2[i][1]) * (point[0] - points2[i][0]))
 
-	# convert lists to numpy arrays
-	points1 = np.asarray(points1)
-	points2 = np.asarray(points2)
+	# convert list of ratios to numpy type
+	ratios = np.asarray(ratios)
 
+	# filter points based on ratios
+	d = np.abs(ratios - np.median(ratios))
+	mdev = np.median(d)
+	s = d/mdev if mdev else 0
+	points1 = np.asarray(points1)[s<1]
+	points2 = np.asarray(points2)[s<1]
+	matches = np.asarray(matches)[s<1] if len(np.asarray(matches)[s<1]) > 1 else np.asarray(matches)[s<1][0]	#h2, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 100)
+	'''
+	h, mask_new = cv2.findHomography(points1, points2, cv2.RANSAC)
+	h2, mask2 = cv2.findHomography(points1, points2, cv2.LMEDS, mask = mask_new)
+
+	'''
+	# convert numpy arrays to list
+	points1 = points1.tolist()
+	points2 = points2.tolist()
+
+	# determine ratio of y change to x change for each set of points
+	ratios = list()
+
+	# iterate through list of points
+	for i, point in enumerate(points1):
+		# calculate ratio
+		ratios.append((point[1] - points2[i][1]) * (point[0] - points2[i][0]))
+
+	# convert list of ratios to numpy type
+	ratios = np.asarray(ratios)
+
+	# filter points based on ratios
+	d = np.abs(ratios - np.median(ratios))
+	mdev = np.median(d)
+	s = d/mdev if mdev else 0
+	points1 = np.asarray(points1)[s<1]
+	points2 = np.asarray(points2)[s<1]
+	matches = np.asarray(matches)[s<1] if len(np.asarray(matches)[s<1]) > 1 else np.asarray(matches)[s<1][0]
+	'''
+	#imgMatches = cv2.drawMatches(img, list(kp1), template, list(kp2), list(matches), None)
+	'''
 	# find homography
-	h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+	#h, mask = cv2.findHomography(points1, points2, cv2.RANSAC, 1) #, cv2.LMEDS)
+	h, mask = cv2.findHomography(points1, points2, cv2.RANSAC, )
+	h, mask = cv2.findHomography(points1, points2, cv2.LMEDS, mask = mask)
 
+	print h
+	'''
 	# use homography
 	height, width, channels = template.shape
-	imgReg = cv2.warpPerspective(img, h, (width, height))
+	imgReg = cv2.warpPerspective(img, h2, (width, height))
 
-	return imgReg, h, imgMatches
+	return imgReg, h2, imgMatches
+
+def alignImages2(img, template):
+
+	# convert images to grayscale
+	im1_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	im2_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+	# Find size of image1
+	sz = im2_gray.shape
+
+	# Define the motion model
+	warp_mode = cv2.MOTION_TRANSLATION
+
+	# Define 2x3 or 3x3 matrices and initialize the matrix to identity
+	if warp_mode == cv2.MOTION_HOMOGRAPHY :
+	    warp_matrix = np.eye(3, 3, dtype=np.float32)
+	else :
+	    warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+	# Specify the number of iterations.
+	number_of_iterations = 5000;
+
+	# Specify the threshold of the increment
+	# in the correlation coefficient between two iterations
+	termination_eps = 1e-10;
+
+	# Define termination criteria
+	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+
+	# Run the ECC algorithm. The results are stored in warp_matrix.
+	(cc, warp_matrix) = cv2.findTransformECC (im1_gray,im2_gray,warp_matrix, warp_mode, criteria)
+
+	if warp_mode == cv2.MOTION_HOMOGRAPHY :
+	    # Use warpPerspective for Homography
+	    im2_aligned = cv2.warpPerspective (img, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+	else :
+	    # Use warpAffine for Translation, Euclidean and Affine
+	    im2_aligned = cv2.warpAffine(img, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+
+	return im2_aligned
