bf92b2db8cc87e455ae2e2be247c6d074279bd13
NRCANTerry <43354925+NRCANTerry@users.noreply.github.com>
2018-10-15 13:12:59 -0400

    Function to overlay template roi onto registered images

---
+++ includes/overlay_roi.py
@@ -0,0 +1,36 @@
+# import necessary modules
+import cv2
+from progress_bar import progress
+
+# optional function to overlay template roi (stake and blob bounding boxes)
+# onto registered images
+def overlay(imgs, templateIntersections, roiCoordinates, upperBorder, img_names, debug_directory):
+    # number of images
+    num_images = len(imgs)
+
+    # iterate through images
+    for count, img_ in enumerate(imgs):
+        # update progress bar
+        progress(count + 1, num_images, status=img_names[count])
+
+        # create write copy of image
+        img_write = img_.copy()
+
+        # iterate through stakes
+        for j, stake in enumerate(roiCoordinates):
+            # overaly template intersection point
+            cv2.circle(img_write, (int(templateIntersections[j][0]),
+                int(templateIntersections[j][1] - upperBorder)), 5, (0,255,0), 3)
+
+            # iterate through roi in each stake
+            for i, rectangle in enumerate(stake):
+                # stake itself
+                if(i == 0):
+                    cv2.rectangle(img_write, (rectangle[0][0], rectangle[0][1]-upperBorder),
+                        (rectangle[1][0], rectangle[1][1]-upperBorder), (0, 0, 255), 3)
+                # blobs
+                else:
+                    cv2.rectangle(img_write, (rectangle[0][0], rectangle[0][1]-upperBorder),
+                        (rectangle[1][0], rectangle[1][1]-upperBorder), (0, 255, 0), 3)
+
+        cv2.imwrite(debug_directory + img_names[count], img_write)
